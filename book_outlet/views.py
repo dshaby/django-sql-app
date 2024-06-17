f68eaf922f9a0ef5c023195045728c5ae0700af7
Daniel Shaby <dshaby@berkeley.edu>
2024-06-16 19:16:22 -0700

    renders book-detail page from DB

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 
@@ -6,4 +6,14 @@ def index(request):
     books = Book.objects.all()
     return render(request, "book_outlet/index.html", {
         "books": books
+    })
+
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling,
     })
